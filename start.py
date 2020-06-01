4654cf295c537a7eaa22a2d24923a22ed7cf2ece
AmmyR <63977585+AmmyR@users.noreply.github.com>
2020-06-01 18:13:08 -0500

    Update start.py

--- start.py
+++ start.py
@@ -6,8 +6,18 @@ from threading import Thread
 from src.Podrum.Server import Server
 
 if __name__ == "__main__":
-    if(sys.argv[1] == "--no_wizard"):
-        serverThread = Thread(target=Server, args=(getcwd(), False))
+    if len(sys.argv) >= 3:
+        if sys.argv[1] == "--no_wizard" and sys.argv[2] == "--travis":
+                serverThread = Thread(target=Server, args=(getcwd(), False, True))
+        else:
+            print("[!] None valid args selected.")
+            serverThread = Thread(target=Server, args=(getcwd(), True))
+    elif len(sys.argv) == 2:
+        if sys.argv[1] == "--no_wizard":
+                serverThread = Thread(target=Server, args=(getcwd(), False))
+        else:
+            print("[!] None valid args selected.")
+            serverThread = Thread(target=Server, args=(getcwd(), True))
     else:
         serverThread = Thread(target=Server, args=(getcwd(), True))
     serverThread.start()
