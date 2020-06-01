e933f239d117470292b52422cdd2557c48975bcf
AmmyR <63977585+AmmyR@users.noreply.github.com>
2020-06-01 17:22:15 -0500

    Update start.py

--- start.py
+++ start.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
+import sys
 from os import getcwd
 from threading import Thread
 from src.Podrum.Server import Server
 
 if __name__ == "__main__":
-    serverThread = Thread(target=Server, args=(getcwd(),))
+    if(sys.argv[1] == "--no_wizard"):
+        serverThread = Thread(target=Server, args=(getcwd(), False))
+    else:
+        serverThread = Thread(target=Server, args=(getcwd(), True))
     serverThread.start()
