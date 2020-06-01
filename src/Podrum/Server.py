173146f9efe2bf5464cda3c2156db107d469c885
AmmyR <63977585+AmmyR@users.noreply.github.com>
2020-06-01 18:29:45 -0500

    Update Server.py

--- src/Podrum/Server.py
+++ src/Podrum/Server.py
@@ -51,6 +51,7 @@ class Server:
         handler.sendOption("name", "MCPE;Podrum powered server;390;1.14.60;0;0;0;PodrumPoweredServer;0")
         if(isTravisBuild):
             print("Build success.")
+            Utils.killServer()
             sys.exit(0)
         while wizard.isInWizard == False:
             cmd = input('> ')
